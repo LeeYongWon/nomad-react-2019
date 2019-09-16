2fadaa09b34dff8b4cc79d2c942710efc0200700
LeeYongwon <dyddnjs0982@naver.cpm>
2019-09-17 00:21:42 +0900

    setState 사용방법

--- nomad-react-2019/src/App.js
+++ nomad-react-2019/src/App.js
@@ -1,58 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const food = [
-  {
-    id:1,
-    name: "kimchi",
-    image: "https://image.shutterstock.com/image-photo/group-young-friends-having-fun-600w-287674133.jpg",
-    rating:4.4
-  },
-  {
-    id:2,
-    name: "hamburger",
-    image: "https://image.shutterstock.com/image-photo/young-girl-listening-music-headphones-600w-1189716400.jpg",
-    rating:5.0
-  },
-  {
-    id:3,
-    name: "불고기",
-    image: "https://image.shutterstock.com/image-photo/beautiful-orange-red-autumn-forest-600w-464920574.jpg",
-    rating:3.2
+
+
+
+class App extends Component {
+  state = {
+    count: 0
+  };
+
+  add = () => {
+    this.setState({
+      count:this.state.count+1
+    })
   }
-];
+  minus = () =>{
+    this.setState({
+      count:this.state.count-1
+    })
+  }
+  render() {
+    return (
+      <div>
+        <div>The number :  {this.state.count}</div>
+        <button onClick={this.add}>+</button>
 
+        <button onClick={this.minus}>-</button>
+      </div>
+    );
+  }
+}
 
-//함수형 component
-function Food({name, image, rating}) {
-  return (
-    <>
-      <h1>i like {name}</h1>
-      <p>{rating}/5.0</p>
-      <img src={image} alt={name}></img>
-      
-    </>
-  );
-}
-Food.propTypes ={
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
-function App() {
-  return (
-    <div className="App">
-      <h2>hello</h2>
-      {food.map(value=>{
-        return <Food 
-        name={value.name} 
-        image={value.image} 
-        key={value.id} 
-        rating={value.rating}
-        />
-      })}
-    </div>
-  );
-}
 
 export default App;
