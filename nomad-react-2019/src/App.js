0f3aafbb4bdcd3a1121bb0285b07e74cd4d93a7d
LeeYongwon <dyddnjs0982@naver.cpm>
2019-09-16 20:42:20 +0900

    props 넘기는 방법 연습

--- nomad-react-2019/src/App.js
+++ nomad-react-2019/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
+const food = [
+  { korean: "kimchi" }, { america: "hamburger" }
+]
+
+function Potato(props) {
+  console.log(props);
+  return (
+    <>
+      <h2>{props.korean}</h2>
+      <h2>{props.america}</h2>
+    </>
+  );
+}
 function App() {
   return (
     <div className="App">
       <h2>hello</h2>
-
+      <Potato korean={food[0].korean} america={food[1].america} />
     </div>
   );
 }
