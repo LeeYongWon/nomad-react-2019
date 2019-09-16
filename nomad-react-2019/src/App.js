9295c74accd6ab60da262b3d3ba2aae48978997a
LeeYongwon <dyddnjs0982@naver.cpm>
2019-09-16 20:55:35 +0900

    class형, arrow형, 일반함수형으로 component 생성 해봤음

--- nomad-react-2019/src/App.js
+++ nomad-react-2019/src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 const food = [
   { korean: "kimchi" }, { america: "hamburger" }
 ]
-
-function Potato(props) {
-  console.log(props);
+//클래스형 component
+class Potato2 extends Component{
+  render(){
+    return(
+      <div>
+        <h2>{this.props.korean}</h2>
+        <h2>{this.props.america}</h2>
+      </div>
+    );
+  }
+}
+//함수형 component
+function Potato({korean, america}) {
   return (
     <>
-      <h2>{props.korean}</h2>
-      <h2>{props.america}</h2>
+      <h2>i like {korean}</h2>
+      <h2>i like {america}</h2>
     </>
+  );
+}
+//arrow function Component
+const Potato3  = ({korean}) =>{
+  return(
+    <div>
+      <h2>Arrow Function Component</h2>
+      {korean}
+    </div>
   );
 }
 function App() {
@@ -18,6 +37,8 @@ function App() {
     <div className="App">
       <h2>hello</h2>
       <Potato korean={food[0].korean} america={food[1].america} />
+      <Potato2 korean={food[0].korean} america={food[1].america} />
+      <Potato3 korean={food[0].korean} america={food[1].america} />
     </div>
   );
 }
